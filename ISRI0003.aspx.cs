a3181b3974f135bdedf12d61ba093b520449664a
jk60huang <alex60.huang@gmail.com>
2023-10-05 14:07:15 +0800

    20231005 1407 Update   ISRI0002 ISRI0003 By Alex

--- ISRI0003.aspx.cs
+++ ISRI0003.aspx.cs
@@ -12,6 +12,7 @@ using System.Text;
 using System.Web.Services;
 using System.Web;
 
+
 namespace ISRE
 {
 
@@ -19,11 +20,33 @@ namespace ISRE
     {
         public static readonly int _ConnectionTimeout = 10000;
         public static readonly IDbConnection _dbConn = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
-
+        
+        public string SESSIONGUID =string.Empty;
+        public string ActioinName = string.Empty;
+        public string sSelected = string.Empty;
+        public dynamic Model = null;
+        public dynamic SESS_LOC = null;
+        public dynamic List_CityList = null;
+   
 
 
         protected void Page_Load(object sender, EventArgs e)
         {
+            
+            var GetSESSIONGUID = Request.QueryString["SESSIONGUID"] ?? "";
+            var GetGUID = Request.QueryString["GUID"] ?? "";
+
+            //SESSIONGUID = GetSESSIONGUID;1
+            //dynamic Model1 = Process_Session(SESSIONGUID);
+
+            SESSIONGUID = Request.QueryString["SESSIONGUID"] ?? "";  /////////SESSIONGUID=session guid
+            ActioinName = (SESSIONGUID == "") ? "新增" : "編輯";
+
+            Model = Process_Session(SESSIONGUID);
+            sSelected = "";
+
+            SESS_LOC = Request["SESS_LOC"] ?? "";
+            List_CityList = StaticQueryDB("Home_ISRE_ACTIVITY_MAIN", "CityList");
 
         }
         protected static List<dynamic> StaticQueryDB(string SPName, string QueryMode)
@@ -31,26 +54,39 @@ namespace ISRE
             DynamicParameters param = new DynamicParameters();
             param.Add("@QueryMode", QueryMode, DbType.String, ParameterDirection.Input);
 
-            List<dynamic> result = _dbConn.Query<dynamic>(
-           SPName,
-            param,
-            commandType: CommandType.StoredProcedure
-            , commandTimeout: _ConnectionTimeout)
-            .ToList();
+            List<dynamic> result = new List<dynamic>();
+
+            using (IDbConnection _dbConn = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            {
+                result = _dbConn.Query<dynamic>(
+                   SPName,
+                    param,
+                    commandType: CommandType.StoredProcedure
+                    , commandTimeout: _ConnectionTimeout)
+                    .ToList();
+
+            }
+
             return result;
+
         }
         protected ISRE_ACTIVITY_MAIN Process_ActivityInfo(String GUID)
         {
             DynamicParameters param = new DynamicParameters();
             param.Add("@GUID", GUID, DbType.String, ParameterDirection.Input);
             param.Add("@QueryMode", "R", DbType.String, ParameterDirection.Input);
+            ISRE_ACTIVITY_MAIN model = new ISRE_ACTIVITY_MAIN();
 
-            ISRE_ACTIVITY_MAIN model = _dbConn.Query<ISRE_ACTIVITY_MAIN>(
-            "Home_ISRE_ACTIVITY_MAIN",
-            param,
-            commandType: CommandType.StoredProcedure
-            , commandTimeout: _ConnectionTimeout)
-            .FirstOrDefault();
+
+            using (IDbConnection _dbConn = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            {
+                model = _dbConn.Query<ISRE_ACTIVITY_MAIN>(
+                        "Home_ISRE_ACTIVITY_MAIN",
+                        param,
+                        commandType: CommandType.StoredProcedure
+                        , commandTimeout: _ConnectionTimeout)
+                        .FirstOrDefault();
+            }
 
             return model;
         }
@@ -66,13 +102,17 @@ namespace ISRE
             DynamicParameters param = new DynamicParameters();
             param.Add("@GUID", GUID, DbType.String, ParameterDirection.Input);
             param.Add("@QueryMode", "R", DbType.String, ParameterDirection.Input);
+            ISRE_SESSION_MAIN model = new ISRE_SESSION_MAIN();
 
-            ISRE_SESSION_MAIN model = _dbConn.Query<ISRE_SESSION_MAIN>(
-            "Session_ISRE_SESSION_MAIN",
-            param,
-            commandType: CommandType.StoredProcedure
-            , commandTimeout: _ConnectionTimeout)
-            .FirstOrDefault();
+            using (IDbConnection _dbConn = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            {
+                model = _dbConn.Query<ISRE_SESSION_MAIN>(
+                        "Session_ISRE_SESSION_MAIN",
+                        param,
+                        commandType: CommandType.StoredProcedure
+                        , commandTimeout: _ConnectionTimeout)
+                        .FirstOrDefault();
+            }
 
             return model;
         }
@@ -133,7 +173,12 @@ namespace ISRE
             foreach (NameValue nv in formVars)
             {
                 // strip out ASP.NET form vars like _ViewState/_EventValidation
-                if (!nv.Name.StartsWith("__") && !nv.Name.EndsWith("_S") && !nv.Name.EndsWith("_E"))
+                //if (!nv.Name.StartsWith("__") && !nv.Name.EndsWith("_S") && !nv.Name.EndsWith("_E"))
+                if (!nv.Name.StartsWith("__") 
+                    && !nv.Name.StartsWith("SESS_DATE_S")
+                    && !nv.Name.StartsWith("SESS_DATE_E")
+                    && !nv.Name.StartsWith("REMIND_MAIL_TEXT")
+                    ) 
                 {
                     sb.Append(nv.Name);
                     sb.AppendLine(": " + HttpUtility.HtmlEncode(nv.Value) + "<br/>");
